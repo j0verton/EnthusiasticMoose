1fe8719f0fd69cca8f6558f9f4a0b78cff06586d
Joe <overtonje@gmail.com>
2020-11-13 09:17:54 -0600

    phase 6

--- Program.cs
+++ Program.cs
@@ -12,6 +12,18 @@ namespace EnthusiasticMoose
             // Let the moose speak!
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
+
+
+            // As a question
+            bool isTrue = MooseAsks("Is Canada real?");
+            if (isTrue)
+            {
+                MooseSays("Really? It seems very unlikely.");
+            }
+            else
+            {
+                MooseSays("I  K N E W  I T !!!");
+            }
         }
         static void MooseSays(string message)
         {
@@ -43,6 +55,27 @@ namespace EnthusiasticMoose
                       \ \'._  ` '_.'
                        `^^` `^^^`
             ");
+        }
+
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
