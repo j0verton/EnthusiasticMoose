657dd9a68f0d1c2b1cb3ddda14d17019f9ff319a
Joe <overtonje@gmail.com>
2020-11-13 15:50:03 -0600

    finshied 8 ball branch

--- Program.cs
+++ Program.cs
@@ -7,6 +7,30 @@ namespace EnthusiasticMoose
     {
         static void Main(string[] args)
         {
+
+            List<string> answers = new List<string> {
+            "As I see it, yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don’t count on it.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Most likely.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Outlook good.",
+            "Reply hazy, try again.",
+            "Signs point to yes.",
+            "Very doubtful.",
+            "Without a doubt.",
+            "Yes.",
+            "Yes – definitely.",
+            "You may rely on it.",
+        };
+
             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
             Console.WriteLine("--------------------------------------------");
 
@@ -14,14 +38,9 @@ namespace EnthusiasticMoose
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("Ask me a question!");
 
-            Random r = new Random();
-            int genRand = r.Next(10, 50);
 
-            // Ask a question
-            SoAsksTheMoose(CanadaQuestion);
-            SoAsksTheMoose(EnthusiasticQuestion);
-            SoAsksTheMoose(LoveCSharpQuestion);
-            SoAsksTheMoose(SecretQuestion);
+            Moose8Ball(answers);
+
         }
 
 
@@ -78,9 +97,19 @@ namespace EnthusiasticMoose
             return answer == "y";
         }
 
-        static void Moose8Ball()
+        static void Moose8Ball(List<string> answers)
         {
+            Console.WriteLine("...well ...you heard the moose... ask a question");
             string answer = Console.ReadLine();
+            while (answer != "")
+            {
+                int genRand = new Random().Next(1, answers.Count);
+                Console.WriteLine(answers[genRand]);
+                Console.WriteLine("go ahead ask another question");
+                answer = Console.ReadLine();
+            };
+
         }
+
     }
 }
